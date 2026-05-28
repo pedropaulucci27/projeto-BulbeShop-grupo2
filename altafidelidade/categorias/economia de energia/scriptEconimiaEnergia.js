0c27bd1554829ae59916ca14ad29bbb1d67ec621
Júlia Leal <julia.borges201@gmail.com>
2026-05-28 09:33:29 -0300

    fix(economia-energia): correções de variáveis

--- altafidelidade/categorias/economia de energia/scriptEconimiaEnergia.js
+++ altafidelidade/categorias/economia de energia/scriptEconimiaEnergia.js
@@ -225,4 +225,4 @@ btnCategorias?.addEventListener("click", (e) => {
 
 fecharMenu?.addEventListener("click", fecharMenuCategorias);
 overlay?.addEventListener("click", fecharMenuCategorias);
-document.addEventListener('DOMContentLoaded', () => renderProdutosCategoria('economia'));
+document.addEventListener('DOMContentLoaded', () => renderProdutosCategoria('Economia de energia'));
